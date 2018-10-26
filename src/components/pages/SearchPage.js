534fa36a344d7e94e4301accd448898a62f3e32b
Ailene-Hoang <aileneh2020@gmail.com>
2018-10-25 21:35:04 -0700

    Moved code from App.js to MainPage.js

--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -8,9 +8,7 @@ class SearchPage extends Component {
 				<div className="search-books-bar">
 					<Link to='/' className="close-search">Close</Link>
 					<div className="search-books-input-wrapper">
-
 						<input type="text" placeholder="Search by title or author"/>
-
 					</div>
 				</div>
 				<div className="search-books-results">
